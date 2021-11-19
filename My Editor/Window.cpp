7263ce78b7cbf415a41b066c523833caf53a9e6e
Praise Codes <73062242+Praisecodes@users.noreply.github.com>
2021-11-19 15:45:39 +0100

    Update Window.cpp

--- My Editor/Window.cpp
+++ My Editor/Window.cpp
@@ -1,21 +1,64 @@
 #include "Window.h"
+Window* window = nullptr;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT mas, WPARAM wp, LPARAM lp) 
+{
+    switch (mas)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            window->EndProcess();
+            break;
+
+        default:
+            return DefWindowProc(hwnd, mas, wp, lp);
+            break;
+    }
+    return 0;
+}
 
 bool Window::Create()
 {
-    return false;
+    WNDCLASS wc = {};
+
+    wc.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpfnWndProc = WindowProc;
+    wc.lpszClassName = L"name";
+
+    RegisterClass(&wc);
+
+    if (!window)
+        window = this;
+
+    CreateWindowEx(0, L"name", L"Text Editor", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, WIDTH, HEIGHT, NULL, NULL, NULL, NULL);
+
+    IsRunning = true;
+
+    return true;
 }
 
 bool Window::isRunning()
 {
-    return false;
+    return IsRunning;
 }
 
 bool Window::EndProcess()
 {
-    return false;
+    IsRunning = false;
+
+    return IsRunning;
 }
 
 bool Window::show()
 {
-    return false;
+    MSG msg = {};
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return true;
 }
